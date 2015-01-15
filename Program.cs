84910ad567618df39fe74b61f980f7017d2bec6d
Antti Järvi <Siilimatkustaja@gmail.com>
2015-01-15 11:33:04 +0200

    Added the DLL:S found to be good and the C# project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GS.Apdu;
+using GS.PCSC;
+using GS.SCard;
+using GS.SCard.Const;
+using GS.Util.Hex;
+using System.Collections.Generic;
+using NdefLibrary;
+
+namespace nfc_rw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
