f6321c36926534fb5c10374f442a66eb51e328d2
Antti Järvi <siilimatkustaja@gmail.com>
2015-01-15 13:08:01 +0200

    changed minor things.

--- Program.cs
+++ Program.cs
@@ -8,15 +8,62 @@ using GS.PCSC;
 using GS.SCard;
 using GS.SCard.Const;
 using GS.Util.Hex;
-using System.Collections.Generic;
+using System.Diagnostics;
 using NdefLibrary;
 
 namespace nfc_rw
 {
     class Program
     {
+        static Dictionary<String, String> APDU_commands = new Dictionary<string, string>(){
+            {"get_card_uid","FF CA 00 00 00"},
+            {"read_binary_blocks","FF B0 00 00 10"} //last two bytes = 1. block to start reading. 2. How many bytes to read
+            };
+
         static void Main(string[] args)
         {
+            ConsoleTraceListener consoleTraceListener = new ConsoleTraceListener();
+            Trace.Listeners.Add(consoleTraceListener);
+
+            PCSCReader reader = new PCSCReader();
+            string input_text = "";
+            while (input_text != "joo")
+            {
+                try
+                {
+                    reader.Connect();
+                    reader.ActivateCard();
+
+                    RespApdu respApdu = reader.Exchange(APDU_commands["get_card_uid"]); // Get Card UID ...
+
+                    if (respApdu.SW1SW2 == 0x9000)
+                    {
+                        Console.WriteLine("UID  = 0x" + HexFormatting.ToHexString(respApdu.Data, true));
+                    }
+
+                    RespApdu testi = reader.Exchange(APDU_commands["read_binary_blocks"]);
+                    if (testi.SW1SW2 == 0x9000)
+                    {
+                        Console.WriteLine("Binary_data  = 0x" + HexFormatting.ToHexString(testi.Data, true));
+                    }
+
+                }
+                catch (WinSCardException ex)
+                {
+                    Console.WriteLine(ex.WinSCardFunctionName + " Error 0x" +
+                                       ex.Status.ToString("X08") + ": " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    reader.Disconnect();
+                    Console.WriteLine("Please press any key...");
+                    input_text = Console.ReadLine();
+                }
+            }
         }
     }
 }
